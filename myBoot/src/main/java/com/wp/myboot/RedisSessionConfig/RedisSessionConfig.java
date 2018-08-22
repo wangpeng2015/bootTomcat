7bca382a58c6d656f90fc33f0c23c48467fd818e
wangpeng2015 <18300247760@163.com>
2018-08-22 13:53:41 +0800

    集成redis

--- myBoot/src/main/java/com/wp/myboot/RedisSessionConfig/RedisSessionConfig.java
+++ myBoot/src/main/java/com/wp/myboot/RedisSessionConfig/RedisSessionConfig.java
@@ -1,8 +1,9 @@
 package com.wp.myboot.RedisSessionConfig;
 
 
-//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-//@EnableRedisHttpSession
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@EnableRedisHttpSession
 public class RedisSessionConfig {
 }
